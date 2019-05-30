b735821149321d8e2e78d260975f3666d5697d4b
Kléber de Oliveira Andrade <pdjkleber@gmail.com>
2019-05-30 13:59:58 -0300

    Bluetooth funcionando

--- GravityCarArduino/GravityCar.cpp
+++ GravityCarArduino/GravityCar.cpp
@@ -1,76 +1,25 @@
 #include "GravityCar.h"
 
-GravityCar::GravityCar(int leftBrakePin, int rightBrakePin, int steeringAnglePin, int leftMotorPin, int rightMotorPin, int fanSpeedPin) {
+GravityCar::GravityCar(int leftBrakePin, int rightBrakePin, int steeringAnglePin) {
   pinMode(leftBrakePin, INPUT);
   pinMode(rightBrakePin, INPUT);
   pinMode(steeringAnglePin, INPUT);
 
-  pinMode(leftMotorPin, OUTPUT);
-  pinMode(rightMotorPin, OUTPUT);
-  pinMode(fanSpeedPin, OUTPUT);
-
   mLeftBrakePin = leftBrakePin;
   mRightBrakePin = rightBrakePin;
   mSteeringAnglePin = steeringAnglePin;
-
-  mLeftMotorPin = leftMotorPin;
-  mRightMotorPin = rightMotorPin;
-  mFanSpeedPin = fanSpeedPin;
-
-  setRightMotor(0);
-  setLeftMotor(0);
-  setFanSpeed(0);
-
-  incomingCommand = "";
 }
 
-void GravityCar::setLeftMotor(int leftMotor) {
-  mLeftMotor = leftMotor;
-  analogWrite(mLeftMotorPin, leftMotor);
-}
+void GravityCar::writeSerialJson(SoftwareSerial bluetooth) {
+  document[JSON_LEFT_BRAKE_TAG] = getLeftBrake();
+  document[JSON_RIGHT_BRAKE_TAG] = getRightBrake();
+  document[JSON_STEERING_ANGLE_TAG] = getSteeringAngle();
 
-void GravityCar::setRightMotor(int rightMotor) {
-  mRightMotor = rightMotor;
-  analogWrite(mRightMotorPin, rightMotor);
-}
+  serializeJson(document, bluetooth);
+  bluetooth.print("\n");
+  bluetooth.flush();
 
-void GravityCar::setFanSpeed(int fanSpeed) {
-  mFanSpeed = fanSpeed;
-}
-
-void GravityCar::writeSerialJson(void) {
-  StaticJsonBuffer<JSON_BUFFER_SIZE> jsonBuffer;
-  JsonObject& object = jsonBuffer.createObject();
-  object[JSON_LEFT_BRAKE_TAG] = getLeftBrake();
-  object[JSON_RIGHT_BRAKE_TAG] = getRightBrake();
-  object[JSON_STEERING_ANGLE_TAG] = getSteeringAngle();
-  object.printTo(Serial);
-  Serial.print("\r\n");
+  serializeJson(document, Serial);
+  Serial.print("\n");
   Serial.flush();
-}
-
-void GravityCar::readSerialJson(void) {
-  
-  while (Serial.available() > 0) {
-
-    char command = Serial.read();
-    incomingCommand += command;
-
-    if (command == '}') {
-
-      DynamicJsonBuffer jsonBuffer;
-      JsonObject& root = jsonBuffer.parseObject(incomingCommand);
-
-      if (!root.success()) {
-        incomingCommand = "";
-        return;
-      }
-      
-      setLeftMotor(root[JSON_LEFT_MOTOR_TAG]);
-      setRightMotor(root[JSON_RIGHT_MOTOR_TAG]);
-      setFanSpeed(root[JSON_FAN_SPEED_TAG]);
-
-      incomingCommand = "";
-    }
-  } 
 }
--- GravityCarArduino/GravityCar.h
+++ GravityCarArduino/GravityCar.h
@@ -2,57 +2,31 @@
 #define __GRAVITY_CAR_H__
 
 #include <ArduinoJson.h>
+#include <SoftwareSerial.h>
 #include "Arduino.h"
 
-#define JSON_BUFFER_SIZE          128
-#define JSON_LEFT_BRAKE_TAG       "LB"
-#define JSON_RIGHT_BRAKE_TAG      "RB"
-#define JSON_STEERING_ANGLE_TAG   "SA"
-
-#define JSON_LEFT_MOTOR_TAG       "LM"
-#define JSON_RIGHT_MOTOR_TAG      "RM"
-#define JSON_FAN_SPEED_TAG        "FA"
+#define JSON_BUFFER_SIZE          64
+#define JSON_LEFT_BRAKE_TAG       "lbrake"
+#define JSON_RIGHT_BRAKE_TAG      "rbrake"
+#define JSON_STEERING_ANGLE_TAG   "sangle"
 
 class GravityCar {
   public:
-    GravityCar(int leftBrakePin, int rightBrakePin, int steeringAnglePin, int leftMotorPin, int rightMotorPin, int fanSpeedPin);
+    GravityCar(int leftBrakePin, int rightBrakePin, int steeringAnglePin);
 
-    void writeSerialJson(void);
-    void readSerialJson(void);
+    void calibrate();
+    void writeSerialJson(SoftwareSerial bluetooth);
 
     int getLeftBrake() const { return analogRead(mLeftBrakePin); }
     int getRightBrake() const { return analogRead(mRightBrakePin); }
     int getSteeringAngle() const { return analogRead(mSteeringAnglePin); }
-
-    int getLeftMotor() const { return mLeftMotor; }
-    int getRightMotor() const { return mRightMotor; }
-    int getFanSpeed() const { return mFanSpeed; }
-
-    String getIncomingCommand() const { return incomingCommand; }
-
-    void setLeftMotor(int leftMotor);
-    void setRightMotor(int rightMotor);
-    void setFanSpeed(int fanSpeed);
 
   private:
     int mLeftBrakePin;
     int mRightBrakePin;
     int mSteeringAnglePin;
 
-    int mLeftMotorPin;
-    int mRightMotorPin;
-    int mFanSpeedPin;
-
-    int mLeftBrakeOffset;
-    int mRightBrakeOffset;
-    int mSteeringAngleOffset;
-
-    int mLeftMotor;
-    int mRightMotor;
-    
-    volatile int mFanSpeed;
-
-    String incomingCommand;
+    StaticJsonDocument<JSON_BUFFER_SIZE> document;
 };
 
 #endif  /* __GRAVITY_CAR_H__ */
